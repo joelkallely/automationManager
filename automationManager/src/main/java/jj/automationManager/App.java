187d823722918b6c0c6dc3d3fca7e9ebe24edfb0
joel.jose <joel.jose@FLY-LP457.flytxt.com>
2018-06-21 20:20:41 +0530

    initial commit

---
+++ automationManager/src/main/java/jj/automationManager/App.java
@@ -0,0 +1,13 @@
+package jj.automationManager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
